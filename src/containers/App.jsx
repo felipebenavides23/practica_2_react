3a3a56e09432315145e831d005c7ba44ca56df4d
Castle <felipebenavides03@gmail.com>
2020-10-30 20:12:23 -0500

    implementacion del buscador y estilo

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import Search from '../components/Search';
 import '../assets/styles/App.scss'
+
 
 
 const app = () => (
     <div className='app'>
         <Header/>
+        <Search/>
     </div>
 
 );
