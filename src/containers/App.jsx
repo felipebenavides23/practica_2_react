e3b6b592be4a66a7948fd7966c04ac289471961f
Castle <felipebenavides03@gmail.com>
2020-10-30 19:55:10 -0500

    implementacion de pagian de videos

---
+++ src/containers/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+import '../assets/styles/App.scss'
+
+
+const app = () => (
+    <div className='app'>
+        <Header/>
+    </div>
+
+);
+
+ export default app;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Helloworld from './components/HelloWorld'
-import Prueba from './components/HelloWorld'
+import App from './containers/App';
+import Header from './components/Header'
 ReactDOM.render(
-<Helloworld/>,document.getElementById('app')
-
-
+<App />,document.getElementById('app')
 );
 
