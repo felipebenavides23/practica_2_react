631a7183767e383a7251b359e87a20b4d4a08abc
Castle <felipebenavides03@gmail.com>
2020-11-04 10:36:28 -0500

    teminacion de estructuray estilos de la pagina

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+
  import '../assets/styles/components/header.scss'
+ import Logo from '../assets/static/logo-platzi-video-BW2.png'
+ import userLogo from '../assets/static/user-icon.png'
 
 
 const header = ()=> (
 
     <header className="header">
-    <img className="header__img" src="../assets/logo-platzi-video-BW2.png" alt="Platzi Video"/>
+    <img className="header__img" src={Logo} alt="Platzi Video"/>
     <div className="header__menu">
       <div className="header__menu--profile">
-        <img src="../assets/user-icon.png" />
+        <img src={userLogo} />
         <p>Perfil</p>
       </div>
       <ul>
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,13 +2,53 @@ import React from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import '../assets/styles/App.scss'
+import Categorie from '../components/categories';
+import Carrusel from '../components/carrusel';
+import CarruselItem from '../components/carruselitem';
+import Footer from '../components/Footer'
 
-
+var titulo1 = 'seccion1 variable'
 
 const app = () => (
     <div className='app'>
         <Header/>
         <Search/>
+
+        <Categorie title={titulo1}>
+            <Carrusel>
+                <CarruselItem />
+                <CarruselItem />
+                <CarruselItem />
+                <CarruselItem />
+                <CarruselItem />
+
+            </ Carrusel>
+        </ Categorie>
+
+        <Categorie title='seccion 2'>
+            <Carrusel>
+                <CarruselItem />
+                <CarruselItem />
+                <CarruselItem />
+                <CarruselItem />
+                <CarruselItem />
+
+            </ Carrusel>
+        </ Categorie>
+
+        <Categorie title ='seccion 3'>
+            <Carrusel>
+                <CarruselItem />
+                <CarruselItem />
+                <CarruselItem />
+                <CarruselItem />
+                <CarruselItem />
+
+            </ Carrusel>
+        </ Categorie>
+        
+
+        <Footer/> 
     </div>
 
 );
