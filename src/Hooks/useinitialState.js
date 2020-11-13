f3cce075a9c6c2b053bd58af8f90197ffa210dc6
Castle <felipebenavides03@gmail.com>
2020-11-13 13:53:52 -0500

    final del curso de react

---
+++ src/Hooks/useinitialState.js
@@ -0,0 +1,16 @@
+import {useState,useEffect} from 'react'
+
+
+const useinitialState = (API) => {
+    const[video , setVideo] = useState({mylist:[],trends:[],originals:[]})
+
+    useEffect(()=>{
+        fetch(API)
+        .then(response => response.json())
+        .then(data => setVideo(data))
+    },[]);
+
+    return( video);
+}
+
+export default useinitialState ;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import '../assets/styles/App.scss'
@@ -6,51 +6,43 @@ import Categorie from '../components/categories';
 import Carrusel from '../components/carrusel';
 import CarruselItem from '../components/carruselitem';
 import Footer from '../components/Footer'
+import useInitialState  from '../Hooks/useinitialState.js'
 
-var titulo1 = 'seccion1 variable'
+const API ='http://localhost:3000/initialState';
 
-const app = () => (
-    <div className='app'>
-        <Header/>
-        <Search/>
+const app = () => {
+        const initialState = useInitialState(API)
+         return(
+     <div className='app'>
+         <Header/>
+         <Search/>
+            {initialState.mylist.length > 0 &&(
+                <Categorie title='mi lista'>
+             <Carrusel>
+                {initialState.mylist.map(item => <CarruselItem key={item.id} {...item} />)}
+                </ Carrusel>
+         </ Categorie>
+            )}
 
-        <Categorie title={titulo1}>
-            <Carrusel>
-                <CarruselItem />
-                <CarruselItem />
-                <CarruselItem />
-                <CarruselItem />
-                <CarruselItem />
+         
 
-            </ Carrusel>
-        </ Categorie>
+         <Categorie title='seccion 2'>
+             <Carrusel>
+                 {initialState.trends.map( item => <CarruselItem key = {item.id } {...item}/>)}
+             </ Carrusel>
+         </ Categorie>
 
-        <Categorie title='seccion 2'>
-            <Carrusel>
-                <CarruselItem />
-                <CarruselItem />
-                <CarruselItem />
-                <CarruselItem />
-                <CarruselItem />
+         <Categorie title ='seccion 3'>
+             <Carrusel>
+             {initialState.originals.map( item => <CarruselItem key = {item.id } {...item}/>)}
 
-            </ Carrusel>
-        </ Categorie>
-
-        <Categorie title ='seccion 3'>
-            <Carrusel>
-                <CarruselItem />
-                <CarruselItem />
-                <CarruselItem />
-                <CarruselItem />
-                <CarruselItem />
-
-            </ Carrusel>
-        </ Categorie>
+             </ Carrusel>
+         </ Categorie>
         
 
-        <Footer/> 
-    </div>
+         <Footer/> 
+     </div>
 
-);
-
- export default app;
+     );
+ }
+  export default app;
